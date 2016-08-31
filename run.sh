633ad6c280c87f9cef53554430d40ef3edc99a2a
Hector Corral <hector.corral@epitech.eu>
2016-08-31 12:20:18 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-echo "Entering VirtualEnv"
+echo "--> Entering VirtualEnv"
 source .env/bin/activate
-echo "Starting Bot..."
-echo "Miller is ALIVE"
+echo "--> Starting Bot..."
+echo "--> Miller is ALIVE!"
 echo "Press Crtl + C to stop the bot..."
 ./bot/run.py
-echo "Bot stoped."
-echo "Leaving VirtualEnv..."
+echo "--> Bot stoped."
+echo "--> Leaving VirtualEnv..."
 deactivate
-echo "Done."
+echo "--> Done."
