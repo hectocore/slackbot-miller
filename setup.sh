d19b431849e2bf169e5e751dc6415ddcd69a0182
Hector Corral <hector.corral@epitech.eu>
2016-09-09 15:38:08 +0200

    Add Python3 to run.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "$(tput setaf 3)--> Warning! python 3 and virtualenv must be installed on y
 echo "$(tput setaf 4)--> Updating slackbot-miller from git$(tput sgr 0)"
 git pull
 echo "$(tput setaf 4)--> Creating VirtualEnv .env$(tput sgr 0)"
-virtualenv .env
+virtualenv .env -p python3
 echo "$(tput setaf 4)--> Entering VirtualEnv$(tput sgr 0)"
 source .env/bin/activate
 echo "$(tput setaf 4)--> Installing Python plugins$(tput sgr 0)"
